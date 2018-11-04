afa4cbc4b4d78009b2477f0283c5809817986884
Orlando Correa <orlandocorrea12@yahoo.com>
2018-11-04 13:19:03 -0500

    finished at last

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,23 +8,38 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var database = firebase.database();
-var playerTurn = database.ref();
-var players = database.ref("/players");
-var player1 = database.ref("/players/player1");
-var player2 = database.ref("/players/player2");
-var playerChat = database.ref("/chat");
+var playerTurn = firebase.database().ref();
+
+var players = firebase.database().ref("/players");
+
+var player1 = firebase.database().ref("/players/player1");
+
+var player2 = firebase.database().ref("/players/player2");
+
+var playerChat = firebase.database().ref("/chat");
+
 var player;
-var p1snapshot;
-var p2snapshot;
+
+var p1Snap;
+
+var p2Snap;
+
 var p1result;
+
 var p2result;
+
 var p1 = null;
+
 var p2 = null;
+
 var wins1 = 0;
+
 var wins2 = 0;
+
 var losses1 = 0;
+
 var losses2 = 0;
+
 var playerNum = 0;
 
 $("#playerInfo").html("<input id=playerName type=text placeholder='Enter your name to begin'><input id=enterPlayer type=submit value=Start>");
@@ -36,7 +51,8 @@ player1.on("value", function(snapshot) {
 		losses1 = snapshot.val().losses;
 		$("#p1name").html("<h2>" + p1 + "</h2>");
 		$("#p1stats").html("<p>Wins: " + wins1 + "  Losses: " + losses1 + "</p>");
-	} else {
+	} 
+	else {
 		$("#p1name").html("Waiting for Player 1");
 		$("#p1stats").empty();
 		
@@ -48,10 +64,7 @@ player1.on("value", function(snapshot) {
 			});
 		};
 	};
-}, function(errorObject) {
-	console.log("The read failed: " + errorObject.code);
-});
-
+}, 
 player2.on("value", function(snapshot) {
 	if (snapshot.val() !== null) {
 		p2 = snapshot.val().player;
@@ -59,7 +72,8 @@ player2.on("value", function(snapshot) {
 		losses = snapshot.val().losses;
 		$("#p2name").html("<h2>" + p2 + "</h2>");
 		$("#p2stats").html("<p>Wins: " + wins2 + "  Losses: " + losses2 + "</p>");
-	} else {
+	} 
+	else {
 		$("#p2name").html("Waiting for Player 2");
 		$("#p2stats").empty();
 	
@@ -71,25 +85,22 @@ player2.on("value", function(snapshot) {
 			});
 		};
 	};
-}, function(errorObject) {
-	console.log("The read failed: " + errorObject.code);
-});
+},
 
 $("#enterPlayer").on("click", function() {
 	event.preventDefault();
 	player = $("#playerName").val().trim();
 	player1.once("value", function(snapshot) {
-		p1snapshot = snapshot;
+		p1Snap = snapshot;
 	}, function(errorObject) {
 		console.log("The read failed: " + errorObject.code);
 	});
 	player2.once("value", function(snapshot) {
-		p2snapshot = snapshot;
+		p2Snap = snapshot;
 	}, function(errorObject) {
 		console.log("The read failed: " + errorObject.code);
 	});
-
-	if (!p1snapshot.exists()) {
+	if (!p1Snap.exists()) {
 		
 		playerNum = 1;
 		
@@ -102,11 +113,11 @@ $("#enterPlayer").on("click", function() {
 		});
 		$("#playerInfo").html("Hi " + player + "! You are Player 1");
 		
-		if (!p2snapshot.exists()) {
+		if (!p2Snap.exists()) {
 			$("#playerTurn").html("Waiting for Player 2 to join...");
 		};
-	
-	} else if (!p2snapshot.exists()) {
+	} 
+	else if (!p2Snap.exists()) {
 		
 		playerNum = 2;
 		
@@ -124,10 +135,11 @@ $("#enterPlayer").on("click", function() {
 		$("#playerInfo").html("Hi " + player + "! You are Player 2");
 		$("#playerTurn").html("Waiting for " + p1 + " to choose.");
 	
-	} else {
+	} 
+	else {
 		$("#playerInfo").html("Sorry. Two people are already playing");
 	};
-});
+})));
 var rpsResults = function() {
 	
 	player1.once("value", function(snapshot) {
@@ -140,7 +152,6 @@ var rpsResults = function() {
 	}, function(errorObject) {
 		console.log("The read failed: " + errorObject.code);
 	});
-	
 	if (p1result.val() !== null && p2result.val() !== null) {
 		
 		if (p1result.val().choice == p2result.val().choice) {
@@ -148,42 +159,48 @@ var rpsResults = function() {
 			$("#p2choices").html("<br><br><br><h1>" + p2result.val().choice + "</h1>");
 			$("#results").html("<br><br><br><br><br><h1>Tie Game!</h1>");
 		
-		} else if (p1result.val().choice == "Rock" && p2result.val().choice == "Scissors") {
+		} 
+		else if (p1result.val().choice == "Rock" && p2result.val().choice == "Scissors") {
 			$("#p1choices").html("<br><br><br><h1>" + p1result.val().choice + "</h1>");
 			$("#p2choices").html("<br><br><br><h1>" + p2result.val().choice + "</h1>");
 			$("#results").html("<br><br><br><br><br><h1>" + p1 + " wins!</h1>");
 			wins1++;
 			losses2++;
 		
-		} else if (p1result.val().choice == "Rock" && p2result.val().choice == "Paper") {
+		} 
+		else if (p1result.val().choice == "Rock" && p2result.val().choice == "Paper") {
 			$("#p1choices").html("<br><br><br><h1>" + p1result.val().choice + "</h1>");
 			$("#p2choices").html("<br><br><br><h1>" + p2result.val().choice + "</h1>");
 			$("#results").html("<br><br><br><br><br><h1>" + p2 + " wins!</h1>");
 			wins2++;
 			losses1++;
 		
-		} else if (p1result.val().choice == "Paper" && p2result.val().choice == "Scissors") {
+		} 
+		else if (p1result.val().choice == "Paper" && p2result.val().choice == "Scissors") {
 			$("#p1choices").html("<br><br><br><h1>" + p1result.val().choice + "</h1>");
 			$("#p2choices").html("<br><br><br><h1>" + p2result.val().choice + "</h1>");
 			$("#results").html("<br><br><br><br><br><h1>" + p2 + " wins!</h1>");
 			wins2++;
 			losses1++;
 		
-		} else if (p1result.val().choice == "Paper" && p2result.val().choice == "Rock") {
+		} 
+		else if (p1result.val().choice == "Paper" && p2result.val().choice == "Rock") {
 			$("#p1choices").html("<br><br><br><h1>" + p1result.val().choice + "</h1>");
 			$("#p2choices").html("<br><br><br><h1>" + p2result.val().choice + "</h1>");
 			$("#results").html("<br><br><br><br><br><h1>" + p1 + " wins!</h1>");
 			wins1++;
 			losses2++;
 		
-		} else if (p1result.val().choice == "Scissors" && p2result.val().choice == "Paper") {
+		} 
+		else if (p1result.val().choice == "Scissors" && p2result.val().choice == "Paper") {
 			$("#p1choices").html("<br><br><br><h1>" + p1result.val().choice + "</h1>");
 			$("#p2choices").html("<br><br><br><h1>" + p2result.val().choice + "</h1>");
 			$("#results").html("<br><br><br><br><br><h1>" + p1 + " wins!</h1>");
 			wins1++;
 			losses2++;
 		
-		} else if (p1result.val().choice == "Scissors" && p2result.val().choice == "Rock") {
+		} 
+		else if (p1result.val().choice == "Scissors" && p2result.val().choice == "Rock") {
 			$("#p1choices").html("<br><br><br><h1>" + p1result.val().choice + "</h1>");
 			$("#p2choices").html("<br><br><br><h1>" + p2result.val().choice + "</h1>");
 			$("#results").html("<br><br><br><br><br><h1>" + p2 + " wins!</h1>");
@@ -196,8 +213,6 @@ var rpsResults = function() {
 			});
 			player1.once("value", function(snapshot) {
 				p1result = snapshot;
-			}, function(errorObject) {
-				console.log("The read failed: " + errorObject.code);
 			});
 			if (p1result.val() !== null) {
 				player1.update({
@@ -207,8 +222,6 @@ var rpsResults = function() {
 			};
 			player2.once("value", function(snapshot) {
 				p2result = snapshot;
-			}, function(errorObject) {
-				console.log("The read failed: " + errorObject.code);
 			});
 			if (p2result.val() !== null) {
 				player2.update({
@@ -231,14 +244,16 @@ playerTurn.on("value", function(snapshot) {
 			$("#p1choices").append("<div>Scissors</div>");
 			$("#playerTurn").html("It's your turn!");
 		
-		} else if (snapshot.val().turn == 2 && playerNum == 2) {
+		} 
+		else if (snapshot.val().turn == 2 && playerNum == 2) {
 			$("#p2choices").empty();
 			$("#p2choices").append("<div>Rock</div>");
 			$("#p2choices").append("<div>Paper</div>");
 			$("#p2choices").append("<div>Scissors</div>");
 			$("#playerTurn").html("It's your turn!");
 		
-		} else if (snapshot.val().turn == 3) {
+		} 
+		else if (snapshot.val().turn == 3) {
 			$("#playerTurn").html("");
 			rpsResults();
 		};
